ee39810d42d676a4bc3029332267e6e9cf923ac3
Anderson <anderson@pop-os.localdomain>
2023-05-09 10:24:37 -0300

    changed route order

--- src/app/useCases/orders/changeOrderStatus.ts
+++ src/app/useCases/orders/changeOrderStatus.ts
@@ -12,10 +12,12 @@ export async function changeOrderStatus(req: Request, res: Response) {
     if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
       return res.status(400).json({
         error: 'Status should be one of these: WAITING, IN_PRODUCTION, DONE'
-      })
+      });
     }
 
+    await Order.findByIdAndUpdate(orderId, { status });
 
+    res.sendStatus(204);
   } catch(e) {
     console.error(e);
     res.sendStatus(500);
